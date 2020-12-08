def7ced33943be9a558a431fbf07b3d3278f7222
Nimesh Nawodana <pwnnawodana@yahoo.com>
2020-12-09 00:32:53 +0530

    Added auto reset key's material to the default after 1s Bug : Having trouble(skip plaing or stop clip and play next) on playing audio clips when press next key(s)

---
+++ Assets/Scripts/Main Scene/KeyMaterialController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyMaterialController : MonoBehaviour
+{
+    Color gameObjectDefaultMaterialColor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var gameObjectRenderer = this.gameObject.GetComponent<Renderer>();
+        gameObjectDefaultMaterialColor = gameObjectRenderer.material.GetColor("_Color");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(delayExecute());
+    }
+    IEnumerator delayExecute()
+    {
+        var currentGameObjectRenderer = this.gameObject.GetComponent<Renderer>();
+
+        if (currentGameObjectRenderer.material.GetColor("_Color") 
+            != gameObjectDefaultMaterialColor)
+        {
+            yield return new WaitForSeconds(1);
+            currentGameObjectRenderer.material
+                .SetColor("_Color", gameObjectDefaultMaterialColor);
+        }
+    }
+}
--- Assets/Scripts/Main Scene/KeyNoteController.cs
+++ Assets/Scripts/Main Scene/KeyNoteController.cs
@@ -23,212 +23,220 @@ public class KeyNoteController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        if (Input.touchCount > 0)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            RaycastHit raycastHit;
-            if (Physics.Raycast(ray, out raycastHit))
+            if (Input.touches[0].phase == TouchPhase.Began)
             {
-                keyName = raycastHit.transform.name;
-                switch (keyName)
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit raycastHit;
+                if (Physics.Raycast(ray, out raycastHit))
                 {
-                    //black
-                    case "black_01":
-                        playKeyTone(true, 1);
-                        break;
-                    case "black_02":
-                        playKeyTone(true, 2);
-                        break;
-                    case "black_03":
-                        playKeyTone(true, 3);
-                        break;
-                    case "black_04":
-                        playKeyTone(true, 4);
-                        break;
-                    case "black_05":
-                        playKeyTone(true, 5);
-                        break;
-                    case "black_06":
-                        playKeyTone(true, 6);
-                        break;
-                    case "black_07":
-                        playKeyTone(true, 7);
-                        break;
-                    case "black_08":
-                        playKeyTone(true, 8);
-                        break;
-                    case "black_09":
-                        playKeyTone(true, 9);
-                        break;
-                    case "black_10":
-                        playKeyTone(true, 10);
-                        break;
-                    case "black_11":
-                        playKeyTone(true, 11);
-                        break;
-                    case "black_12":
-                        playKeyTone(true, 12);
-                        break;
-                    case "black_13":
-                        playKeyTone(true, 13);
-                        break;
-                    case "black_14":
-                        playKeyTone(true, 14);
-                        break;
-                    case "black_15":
-                        playKeyTone(true, 15);
-                        break;
-                    case "black_16":
-                        playKeyTone(true, 16);
-                        break;
-                    case "black_17":
-                        playKeyTone(true, 17);
-                        break;
-                    case "black_18":
-                        playKeyTone(true, 18);
-                        break;
-                    case "black_19":
-                        playKeyTone(true, 19);
-                        break;
-                    case "black_20":
-                        playKeyTone(true, 20);
-                        break;
-                    case "black_21":
-                        playKeyTone(true, 21);
-                        break;
-                    case "black_22":
-                        playKeyTone(true, 22);
-                        break;
-                    case "black_23":
-                        playKeyTone(true, 23);
-                        break;
-                    case "black_24":
-                        playKeyTone(true, 24);
-                        break;
-                    case "black_25":
-                        playKeyTone(false, 25);
-                        break;
+                    keyName = raycastHit.transform.name;
+                    switch (keyName)
+                    {
+                        //black
+                        case "black_01":
+                            playKeyTone(true, 1);
+                            break;
+                        case "black_02":
+                            playKeyTone(true, 2);
+                            break;
+                        case "black_03":
+                            playKeyTone(true, 3);
+                            break;
+                        case "black_04":
+                            playKeyTone(true, 4);
+                            break;
+                        case "black_05":
+                            playKeyTone(true, 5);
+                            break;
+                        case "black_06":
+                            playKeyTone(true, 6);
+                            break;
+                        case "black_07":
+                            playKeyTone(true, 7);
+                            break;
+                        case "black_08":
+                            playKeyTone(true, 8);
+                            break;
+                        case "black_09":
+                            playKeyTone(true, 9);
+                            break;
+                        case "black_10":
+                            playKeyTone(true, 10);
+                            break;
+                        case "black_11":
+                            playKeyTone(true, 11);
+                            break;
+                        case "black_12":
+                            playKeyTone(true, 12);
+                            break;
+                        case "black_13":
+                            playKeyTone(true, 13);
+                            break;
+                        case "black_14":
+                            playKeyTone(true, 14);
+                            break;
+                        case "black_15":
+                            playKeyTone(true, 15);
+                            break;
+                        case "black_16":
+                            playKeyTone(true, 16);
+                            break;
+                        case "black_17":
+                            playKeyTone(true, 17);
+                            break;
+                        case "black_18":
+                            playKeyTone(true, 18);
+                            break;
+                        case "black_19":
+                            playKeyTone(true, 19);
+                            break;
+                        case "black_20":
+                            playKeyTone(true, 20);
+                            break;
+                        case "black_21":
+                            playKeyTone(true, 21);
+                            break;
+                        case "black_22":
+                            playKeyTone(true, 22);
+                            break;
+                        case "black_23":
+                            playKeyTone(true, 23);
+                            break;
+                        case "black_24":
+                            playKeyTone(true, 24);
+                            break;
+                        case "black_25":
+                            playKeyTone(false, 25);
+                            break;
 
-                    // White Keys
-                    case "white_01":
-                        playKeyTone(false, 1);
-                        break;
-                    case "white_02":
-                        playKeyTone(false, 2);
-                        break;
-                    case "white_03":
-                        playKeyTone(false, 3);
-                        break;
-                    case "white_04":
-                        playKeyTone(false, 4);
-                        break;
-                    case "white_05":
-                        playKeyTone(false, 5);
-                        break;
-                    case "white_06":
-                        playKeyTone(false, 6);
-                        break;
-                    case "white_07":
-                        playKeyTone(false, 7);
-                        break;
-                    case "white_08":
-                        playKeyTone(false, 8);
-                        break;
-                    case "white_09":
-                        playKeyTone(false, 9);
-                        break;
-                    case "white_10":
-                        playKeyTone(false, 10);
-                        break;
-                    case "white_11":
-                        playKeyTone(false, 11);
-                        break;
-                    case "white_12":
-                        playKeyTone(false, 12);
-                        break;
-                    case "white_13":
-                        playKeyTone(false, 13);
-                        break;
-                    case "white_14":
-                        playKeyTone(false, 14);
-                        break;
-                    case "white_15":
-                        playKeyTone(false, 15);
-                        break;
-                    case "white_16":
-                        playKeyTone(false, 16);
-                        break;
-                    case "white_17":
-                        playKeyTone(false, 17);
-                        break;
-                    case "white_18":
-                        playKeyTone(false, 18);
-                        break;
-                    case "white_19":
-                        playKeyTone(false, 19);
-                        break;
-                    case "white_20":
-                        playKeyTone(false, 20);
-                        break;
-                    case "white_21":
-                        playKeyTone(false, 21);
-                        break;
-                    case "white_22":
-                        playKeyTone(false, 22);
-                        break;
-                    case "white_23":
-                        playKeyTone(false, 23);
-                        break;
-                    case "white_24":
-                        playKeyTone(false, 24);
-                        break;
-                    case "white_25":
-                        playKeyTone(false, 25);
-                        break;
-                    case "white_26":
-                        playKeyTone(false, 26);
-                        break;
-                    case "white_27":
-                        playKeyTone(false, 27);
-                        break;
-                    case "white_28":
-                        playKeyTone(false, 28);
-                        break;
-                    case "white_29":
-                        playKeyTone(false, 29);
-                        break;
-                    case "white_30":
-                        playKeyTone(false, 30);
-                        break;
-                    case "white_31":
-                        playKeyTone(false, 31);
-                        break;
-                    case "white_32":
-                        playKeyTone(false, 32);
-                        break;
-                    case "white_33":
-                        playKeyTone(false, 33);
-                        break;
-                    case "white_34":
-                        playKeyTone(false, 34);
-                        break;
-                    case "white_35":
-                        playKeyTone(false, 35);
-                        break;
-                    case "white_36":
-                        playKeyTone(false, 36);
-                        break;
+                        // White Keys
+                        case "white_01":
+                            playKeyTone(false, 1);
+                            break;
+                        case "white_02":
+                            playKeyTone(false, 2);
+                            break;
+                        case "white_03":
+                            playKeyTone(false, 3);
+                            break;
+                        case "white_04":
+                            playKeyTone(false, 4);
+                            break;
+                        case "white_05":
+                            playKeyTone(false, 5);
+                            break;
+                        case "white_06":
+                            playKeyTone(false, 6);
+                            break;
+                        case "white_07":
+                            playKeyTone(false, 7);
+                            break;
+                        case "white_08":
+                            playKeyTone(false, 8);
+                            break;
+                        case "white_09":
+                            playKeyTone(false, 9);
+                            break;
+                        case "white_10":
+                            playKeyTone(false, 10);
+                            break;
+                        case "white_11":
+                            playKeyTone(false, 11);
+                            break;
+                        case "white_12":
+                            playKeyTone(false, 12);
+                            break;
+                        case "white_13":
+                            playKeyTone(false, 13);
+                            break;
+                        case "white_14":
+                            playKeyTone(false, 14);
+                            break;
+                        case "white_15":
+                            playKeyTone(false, 15);
+                            break;
+                        case "white_16":
+                            playKeyTone(false, 16);
+                            break;
+                        case "white_17":
+                            playKeyTone(false, 17);
+                            break;
+                        case "white_18":
+                            playKeyTone(false, 18);
+                            break;
+                        case "white_19":
+                            playKeyTone(false, 19);
+                            break;
+                        case "white_20":
+                            playKeyTone(false, 20);
+                            break;
+                        case "white_21":
+                            playKeyTone(false, 21);
+                            break;
+                        case "white_22":
+                            playKeyTone(false, 22);
+                            break;
+                        case "white_23":
+                            playKeyTone(false, 23);
+                            break;
+                        case "white_24":
+                            playKeyTone(false, 24);
+                            break;
+                        case "white_25":
+                            playKeyTone(false, 25);
+                            break;
+                        case "white_26":
+                            playKeyTone(false, 26);
+                            break;
+                        case "white_27":
+                            playKeyTone(false, 27);
+                            break;
+                        case "white_28":
+                            playKeyTone(false, 28);
+                            break;
+                        case "white_29":
+                            playKeyTone(false, 29);
+                            break;
+                        case "white_30":
+                            playKeyTone(false, 30);
+                            break;
+                        case "white_31":
+                            playKeyTone(false, 31);
+                            break;
+                        case "white_32":
+                            playKeyTone(false, 32);
+                            break;
+                        case "white_33":
+                            playKeyTone(false, 33);
+                            break;
+                        case "white_34":
+                            playKeyTone(false, 34);
+                            break;
+                        case "white_35":
+                            playKeyTone(false, 35);
+                            break;
+                        case "white_36":
+                            playKeyTone(false, 36);
+                            break;
 
-                    // Deafult check
-                    case "A":
-                        audioSource.clip = blackAudioClips[Random.Range(0,20)];
-                        audioSource.Play();
-                        break;
-                    default:
-                        break;
+                        // Deafult check
+                        case "A":
+                            audioSource.clip = blackAudioClips[Random.Range(0, 20)];
+                            audioSource.Play();
+                            break;
+                        default:
+                            break;
+                    }
+                    changeMaterial(keyName, true);
                 }
-                changeMaterial(keyName, true);
             }
+            else if (Input.touches[0].phase == TouchPhase.Ended)
+            {
+                changeMaterial(keyName, false);
+            }
+            
         }
     }
 
@@ -251,14 +259,16 @@ public class KeyNoteController : MonoBehaviour
         if (gameObject != null)
         {
             var gameObjectRenderer = gameObject.GetComponent<Renderer>();
+            Color color = gameObjectRenderer.material.GetColor("_Color");
+            gameObjectRenderer.material.SetColor("_Color", Color.red);
+/*
             if (isPressed)
             {
-                gameObjectRenderer.material.SetColor("_Color", Color.red);
             }
             else
             {
-                gameObjectRenderer.material.SetColor("_Color", Color.white);
-            }
+                gameObjectRenderer.material.SetColor("_Color", color);
+            }*/
 
         }        
     }
